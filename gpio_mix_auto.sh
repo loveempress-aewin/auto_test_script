b142c139c159bc5f210445b1e47bf02bbb1b22ce
loveloveempress <love@AEWIN1138.aewin.com>
2026-01-28 16:40:17 +0800

    use if > case XDDD
    because just like love and like thing!!!!

--- gpio_mix_auto.sh
+++ gpio_mix_auto.sh
@@ -36,17 +36,6 @@ bash ${love_env}/gpio_execute.sh
 #### bash expand part
 
 read -p "u need input GPI GPO mapping?" love_expand_iobound
-case $love_expand_iobound in
-    y|Y|yes|YES)
-        echo "yes"
-        ;;
-    *)
-        echo "u not choose"
-        ;;
-esac
-
-
-read -p "u need input GPI GPO mapping?" love_expand_iobound
 if [[ "$love_expand_iobound" =~ ^[Yy]([Ee][Ss])?$ ]]; then
     # echo "Accepted: $love_expand_iobound"
     bash ${love_env}/gpio_expand_iobound.sh
@@ -57,13 +46,3 @@ if [[ -f ${love_env} ./gpio_love_auto/bound_io.csv ]];then
 else
     echo "not have bound_io.csv "
 fi
-
-read -p "u need input GPI GPO mapping?" love_expand_iobound
-case $love_expand_iobound in
-    y|Y|yes|YES)
-        echo "yes"
-        ;;
-    *)
-        echo "u not choose"
-        ;;
-esac
